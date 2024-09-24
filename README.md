5982574c11bbd0cf03a2ec8805661be8126b5d26
JeremieV <jeremievaney@gmail.com>
2024-09-24 18:38:25 +0100

    task 1 complete

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Setup I did right at the start:
 
 ```sh
 npm init # create a new npm project
+npm i puppeteer # install puppeteer
 npm install typescript ts-node @types/node --save-dev # install typescript
 npx tsc --init # setup typescript
 
@@ -13,3 +14,34 @@ git init
 
 ## Task 1
 
+I checked the first three bookmakers at the [provided link](https://www.telegraph.co.uk/betting/sports-guides/best-betting-sites-uk/) and chose bet365.com somewhat arbitrarily but also because the interface looked clean and regular.
+
+I have no experience using puppeteer but I am very familiar with playwright, so I need to read the documentation a little bit.
+
+It took me some time to explore puppeteer and the different bookmakers, but i ended up settling on betmgm.co.uk because it had the easiest markup to work with in my opinion.
+
+If we look at an example URL:
+
+https://www.betmgm.co.uk/sports\#racing/event/1021685425
+
+We see that the UI is made up of rows that contain horse name and fractional odds, among other things. We want to extract each row separately, and then from each row select the name and the odds, so that they do not get scrambled.
+
+A row is a div with the class `KambiBC-racing-participant-outcome-container`. We can then query the rows by using the following query selector:
+
+```
+div.KambiBC-racing-participant-outcome-container
+```
+
+Importantly, before we do so, we need to wait for the page to load. We can wait for the above selector. There is a 30 seconds default timeout.
+
+```js
+await page.waitForSelector(rowsSelector)
+```
+
+We can then select all the rows and extract the name and available odds using puppeteer. Notice that the bookmaker I have chosen does not display the odds other than the UK, Australia and South Africa.
+
+If the country is none of the above, then the only odds displayed are 'SP' ("Starting Price"). In this case, my puppeteer script returns 'SP'. This can be easily changed depending on the desired behaviour (unspecified in the instructions).
+
+Another edge case to take into account is for events that have already finished. In this case, the bookmaker site uses a slightly different markup, and my script times out. For a production application this case should certainly be handled.
+
+## Task 2
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,84 @@
 import puppeteer from 'puppeteer';
 
-async function main() {
-  // Launch the browser and open a new blank page
+// Write a Node.js program using Puppeteer to scrape odds for a given horse racing event
+// from a bookmaker site. The script should take in the following parameters:
+// - eventUrl (string): The URL of the horse racing event page on the bookmaker site.
+// 
+// The script should use Puppeteer to navigate to the event page, scrape the horse name
+// and odds for the event from the bookmaker site, and return them in a JSON format.
+
+interface Output {
+  eventUrl: string;
+  horses: {
+    name: string;
+    /** Odds are in fractional form: "x/y" (e.g. 2/1) */
+    odds: string;
+  }[]
+}
+
+async function scrapeOdds(eventUrl: string): Promise<Output> {
+  // Launch the browser, open a new blank page and navigate to the event URL
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.goto(eventUrl);
+  await page.setViewport({ width: 1080, height: 1024 });
 
-  // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
+  const rowsSelector = `div.KambiBC-racing-participant-outcome-container`;
 
-  // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
+  // Wait for the rows to be loaded. There is a 30 seconds default timeout
+  await page.waitForSelector(rowsSelector)
 
-  // Type into search box
-  await page.type('.devsite-search-field', 'automate beyond recorder');
+  const horses = await page.$$eval(rowsSelector,
+    elements => {
+      return elements.map((element, index) => {
+        const name = element.querySelector('div.KambiBC-racing-participant__name span')?.textContent;
+        if (!name) {
+          throw new Error(`Name not found on row ${index}`);
+        }
+        const odds = element.querySelector('div.KambiBC-racing-outcome-list button:first-of-type div.sc-kAyceB.gIMtGL')?.textContent;
+        if (!odds) {
+          throw new Error(`Odds not found for horse ${name} on row ${index}`);
+        }
+        return { name, odds };
+      });
+    });
 
-  // Wait and click on first result
-  const searchResultSelector = '.devsite-result-item-link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize and automate'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
+  if (!horses) {
+    throw new Error('No horses found');
+  }
 
   await browser.close();
+
+  return {
+    eventUrl,
+    horses
+  }
 };
+
+async function main() {
+  // validate the command line arguments
+  if (process.argv.length !== 3) {
+    console.error('Usage: node index.js <eventUrl>');
+    process.exit(1);
+  }
+  
+  // validate URL
+  const url = process.argv[2];
+  try {
+    // will throw if the URL is invalid
+    new URL(url);
+  } catch {
+    console.error(`Invalid URL: ${url}`);
+    process.exit(1);
+  }
+  // ensure that the URL is a betmgm.co.uk URL
+  if (!(new URL(url).hostname === 'www.betmgm.co.uk')) {
+    throw new Error('Invalid URL: must be a betmgm.co.uk URL');
+  }
+
+  // run the main function (puppeteer)
+  const odds = await scrapeOdds(url)
+  console.log(odds);
+}
 
 main();
